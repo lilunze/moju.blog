5635b7982b5052c59bb5c5acbeaded807b9d84b6
lilunze <lilunze@webadmin.com>
2018-06-28 10:56:41 +0800

    归档

---
+++ src/components/archives.vue
@@ -0,0 +1,15 @@
+<template>
+	<div class="archives"></div>
+</template>
+<script>
+	export default {
+		components:{},
+		data () {
+			return {}
+		},
+		created () {
+
+		}
+	}
+</script>
+<style scoped></style>
