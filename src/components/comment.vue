0ebf332751f34116caf19a81929ca2d88f2eca15
lilunze <lilunze@webadmin.com>
2018-06-28 10:58:25 +0800

    留言评论

---
+++ src/components/comment.vue
@@ -0,0 +1,17 @@
+<template>
+	<div class="comment"></div>
+</template>
+<script>
+	export default {
+		components:{},
+		data () {
+			return {
+
+			}
+		},
+		created () {
+
+		}
+	}
+</script>
+<style scoped></style>
