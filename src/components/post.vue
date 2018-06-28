dec823a08f2f8bd0f91a0cebe5b7bf646b5a9525
lilunze <lilunze@webadmin.com>
2018-06-28 16:56:41 +0800

    文章详情组件

---
+++ src/components/post.vue
@@ -0,0 +1,15 @@
+<template>
+	<div class="post">
+		<h1 class="title"></h1>
+		<p><span class="tag"></span><span class="date"></span></p>
+		<section class="content">
+			<router-view />	
+		</section>
+	</div>
+</template>
+<script>
+	export default {
+
+	}
+</script>
+<style scoped></style>
