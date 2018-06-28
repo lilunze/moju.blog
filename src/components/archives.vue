8bd2cc2225ca13139c83e0dd98a5315650b6ca18
lilunze <lilunze@webadmin.com>
2018-06-28 16:54:23 +0800

    分类

--- src/components/archives.vue
+++ src/components/archives.vue
@@ -1,14 +1,27 @@
 <template>
-	<div class="archives"></div>
+	<div class="archives">
+		<div class="sublist">
+			<ul>
+				<li v-for="v in archives"><router-link :to="v.path">{{v.tag}}</router-link></li>
+			</ul>
+		</div>
+		<keep-alive>
+			<router-view />
+		</keep-alive>
+	</div>
 </template>
 <script>
+	import axios from 'axios'
 	export default {
-		components:{},
 		data () {
-			return {}
+			return {
+				archives:[]
+			}
 		},
 		created () {
-
+			axios.get('/static/db/archives.json').then((res)=>{
+				this.archives=res.data;
+			})
 		}
 	}
 </script>
---
+++ src/components/archives/jquery.vue
@@ -0,0 +1,16 @@
+<template>
+	<div class="jquery">
+		
+	</div>
+</template>
+<script>
+	import ablock from "@/components/ablock"
+	export default {
+		component:{
+			ablock
+		}
+	}
+</script>
+<style scoped>
+	
+</style>
