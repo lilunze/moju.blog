ac48d93535474039a7ce84a1ad7e7a91339bb78d
lilunze <lilunze@webadmin.com>
2018-06-28 10:30:01 +0800

    添加首页

---
+++ src/components/index.vue
@@ -0,0 +1,37 @@
+<template>
+	<div class="index">
+		<nav class="nav">
+			<div class="logo"><img src=""></div>
+			<div class="nav_list"></div>
+		</nav>
+		
+		<section>
+			
+		</section>
+
+		<footer class="copyright">
+			
+		</footer>
+	</div>
+</template>
+<script>
+	export default {
+		data () {
+			return {
+
+			}
+		},
+		created () {
+
+		}
+	}
+</script>
+<style scpoed>
+	.index
+	{
+		width: 100%;
+		height: 100%;
+		background: url(/static/images/background/bg_1.jpg);
+		background-attachment: fixed;
+	}
+</style>
