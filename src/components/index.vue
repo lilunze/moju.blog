d2ffa29ebd2c468bd87c4236bef7239187c6c626
lilunze <lilunze@webadmin.com>
2018-06-28 15:20:04 +0800

    添加pages组件 && 修改导航路径

--- src/components/index.vue
+++ src/components/index.vue
@@ -1,12 +1,29 @@
 <template>
 	<div class="index">
 		<nav class="nav">
-			<div class="logo"><img src=""></div>
-			<div class="nav_list"></div>
+			<div class="logo"><img src="/static/images/logo/logo-m.png"></div>
+			<div class="nav_list">
+				<a href="#" class="icon-github"></a>
+				<a href="#" class="icon-weibo"></a>
+				<a href="#" class="icon-search"></a>
+			</div>
 		</nav>
 		
 		<section>
-			
+			<div class="left"></div>
+			<div class="center">
+				<!-- <p><img src="/static/images/res/avatar.png"></p> -->
+			</div>
+			<div class="right">
+				<ul>
+					<li><router-link to="/blog">Blog</router-link></li>
+					<li><router-link to="/archives">Archives</router-link></li>
+					<li><router-link to="/todo">Todo</router-link></li>
+					<li><router-link to="/comment">Comment</router-link></li>
+					<li><router-link to="/wiki">Wiki</router-link></li>
+					<li><router-link to="/question">Q&A</router-link></li>
+				</ul>
+			</div>
 		</section>
 
 		<footer class="copyright">
---
+++ src/components/pages.vue
@@ -0,0 +1,60 @@
+<template>
+	<div class="pages">
+		<span>&gt;</span>
+		<span v-for="dot in pages">{{dot}}</span>
+		<span>...</span>
+		<span>{{total}}</span>
+		<span>&lt;</span>
+	</div>
+</template>
+<script>
+	export default {
+		data () {
+			return {
+				current:this.currentPage
+			}
+		},
+		props:{
+			// 总数据条数
+			total:{
+				type:Number,
+				default:1
+			},
+			// 每页显示的数据条数
+			display:{
+				type:Number,
+				default:5
+			},
+			// 当前页码
+			currentPage:{
+				type:Number,
+				default:1
+			},
+			// 显示的页码
+			pages:{
+				type:Array,
+				default:[]
+			}
+		},
+		created:{
+
+		},
+		comuted:{
+
+		},
+		methods:{
+			setCurrent (index){
+				console.log(index)
+			},
+			next (){
+				this.current++;
+			},
+			prev (){
+				this.current--;
+			}
+		}
+	}
+</script>
+<style scoped>
+	
+</style>
